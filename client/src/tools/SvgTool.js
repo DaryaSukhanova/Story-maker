e637e6603f232b8e5f3688e6077fd4275937e2f9
Sukhanova Darya <daria_15tit@mail.ru>
2024-01-04 00:20:39 +0300

    added a bounding box for the brush

--- client/src/tools/SvgTool.js
+++ client/src/tools/SvgTool.js
@@ -1,23 +1,39 @@
-export default class SvgTool{
+export default class SvgTool {
     constructor(svgCanvas) {
-        this.svgCanvas = svgCanvas
+        this.svgCanvas = svgCanvas;
         this.currentStroke = '#8DADFF';
-        this.currentLineWidth = '2'
-        this.destroyEvents()
+        this.currentLineWidth = '2';
+        this.destroyEvents();
     }
 
-    set svgFillStroke(color){
-        console.log(color)
-        this.currentStroke = color
+    set svgFillStroke(color) {
+        this.currentStroke = color;
     }
-    set svgLineWidth(width){
-        this.currentLineWidth = width
+    set svgLineWidth(width) {
+        this.currentLineWidth = width;
     }
 
+    destroyEvents() {
+        this.svgCanvas.onmousemove = null;
+        this.svgCanvas.onmousedown = null;
+        this.svgCanvas.onmouseup = null;
+    }
 
-    destroyEvents(){
-        this.svgCanvas.onmousemove = null
-        this.svgCanvas.onmousedown = null
-        this.svgCanvas.onmouseup = null
+    getBoundingBox(element) {
+        const boundingBox = element.getBBox();
+        const svgPoint = this.svgCanvas.createSVGPoint();
+
+        svgPoint.x = boundingBox.x;
+        svgPoint.y = boundingBox.y;
+        const startPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
+
+        svgPoint.x = boundingBox.x + boundingBox.width;
+        svgPoint.y = boundingBox.y + boundingBox.height;
+        const endPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
+        console.log("startPoint.x", startPoint.x, "startPoint.y", startPoint.y)
+        return {
+            width: endPoint.x - startPoint.x,
+            height: endPoint.y - startPoint.y,
+        };
     }
 }
