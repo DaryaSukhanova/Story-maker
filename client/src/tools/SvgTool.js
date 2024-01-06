24c7e6535b4844e4303b0d38c457ad32043ec14e
Sukhanova Darya <daria_15tit@mail.ru>
2024-01-07 00:38:50 +0300

    The logic of the BoxSelect tool has been added

--- client/src/tools/SvgTool.js
+++ client/src/tools/SvgTool.js
@@ -32,42 +32,42 @@ export default class SvgTool {
         this.svgCanvas.onmouseup = null;
     }
 
-    getBoundingBox(element) {
-        const boundingBox = element.getBBox();
-        const svgPoint = this.svgCanvas.createSVGPoint();
-
-        svgPoint.x = boundingBox.x;
-        svgPoint.y = boundingBox.y;
-        const startPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
-
-        svgPoint.x = boundingBox.x + boundingBox.width;
-        svgPoint.y = boundingBox.y + boundingBox.height;
-        const endPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
-
-        let width = endPoint.x - startPoint.x;
-        let height = endPoint.y - startPoint.y;
-
-        // Если есть предыдущий ограничивающий прямоугольник, удаляем его
-        if (this.boundingBoxRect) {
-            this.svgCanvas.removeChild(this.boundingBoxRect);
-        }
-
-        // Создаем новый ограничивающий прямоугольник
-        const rectElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        rectElement.setAttribute("id", "boundingBox")
-        rectElement.setAttribute("x", `${element.getBBox().x}`);
-        rectElement.setAttribute("y", `${element.getBBox().y}`);
-        rectElement.setAttribute("width", `${width}`);
-        rectElement.setAttribute("height", `${height}`);
-        rectElement.setAttribute("stroke", "#003ace");
-        rectElement.setAttribute("fill", "none");
-        rectElement.setAttribute("stroke-width", "0.5");
-
-
-        // Добавляем новый ограничивающий прямоугольник на холст
-        this.svgCanvas.appendChild(rectElement);
-
-        // Сохраняем ссылку на новый ограничивающий прямоугольник
-        this.boundingBoxRect = rectElement;
-    }
+    // getBoundingBox(element) {
+    //     const boundingBox = element.getBBox();
+    //     const svgPoint = this.svgCanvas.createSVGPoint();
+    //
+    //     svgPoint.x = boundingBox.x;
+    //     svgPoint.y = boundingBox.y;
+    //     const startPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
+    //
+    //     svgPoint.x = boundingBox.x + boundingBox.width;
+    //     svgPoint.y = boundingBox.y + boundingBox.height;
+    //     const endPoint = svgPoint.matrixTransform(this.svgCanvas.getScreenCTM().inverse());
+    //
+    //     let width = endPoint.x - startPoint.x;
+    //     let height = endPoint.y - startPoint.y;
+    //
+    //     // Если есть предыдущий ограничивающий прямоугольник, удаляем его
+    //     if (this.boundingBoxRect) {
+    //         this.svgCanvas.removeChild(this.boundingBoxRect);
+    //     }
+    //
+    //     // Создаем новый ограничивающий прямоугольник
+    //     const rectElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    //     rectElement.setAttribute("id", "boundingBox")
+    //     rectElement.setAttribute("x", `${element.getBBox().x}`);
+    //     rectElement.setAttribute("y", `${element.getBBox().y}`);
+    //     rectElement.setAttribute("width", `${width}`);
+    //     rectElement.setAttribute("height", `${height}`);
+    //     rectElement.setAttribute("stroke", "#003ace");
+    //     rectElement.setAttribute("fill", "none");
+    //     rectElement.setAttribute("stroke-width", "0.5");
+    //
+    //
+    //     // Добавляем новый ограничивающий прямоугольник на холст
+    //     this.svgCanvas.appendChild(rectElement);
+    //
+    //     // Сохраняем ссылку на новый ограничивающий прямоугольник
+    //     this.boundingBoxRect = rectElement;
+    // }
 }
