576ce1f7266a5040dafc9e54d2cb661557b6c6ee
Sukhanova Darya <daria_15tit@mail.ru>
2024-03-26 01:15:08 +0300

    added keys from keyframes

--- client/src/components/animation-components/TimelineBlock.jsx
+++ client/src/components/animation-components/TimelineBlock.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../../styles/timeline-block.scss';
-const TimeLineBlock = () => {
+import toolBlockState from "../../store/toolBlockState";
+import {observer} from "mobx-react-lite";
+import {logDOM} from "@testing-library/react";
+const TimeLineBlock = observer (() => {
     const [elapsedTime, setElapsedTime] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
+    const [isRunningThumb, setIsRunningThumb] = useState(false);
     const [thumbPosition, setThumbPosition] = useState(0);
     const [totalTime, setTotalTime] = useState(0);
-    const [thumbTime, setThumbTime] = useState(0);
+    const [keys, setKeys] = useState([])
+    const [isDraggingKey, setIsDraggingKey] = useState(true);
 
     const intervalIdRef = useRef(null);
     const startTimeRef = useRef(null);
     const timelineRef = useRef(null);
     const thumbEndTimeRef = useRef(null);
     const thumbCurrent = useRef(null);
+    const keyRef = useRef(null);
+    const timelineKeyRef = useRef(null);
 
     const formatTime = (time) => {
         const pad = (num) => (num < 10 ? `0${num}` : num);
@@ -22,7 +28,7 @@ const TimeLineBlock = () => {
     };
 
     useEffect(() => {
-        if (isRunning) {
+        if (isRunningThumb) {
             startTimeRef.current = Date.now() - elapsedTime;
             intervalIdRef.current = setInterval(() => {
                 const currentTime = Date.now();
@@ -31,7 +37,7 @@ const TimeLineBlock = () => {
                 if (newElapsedTime >= totalTime) {
                     setElapsedTime(0); // Если достигло, сбрасываем таймер на 0
                     clearInterval(intervalIdRef.current);
-                    setIsRunning(true);
+                    setIsRunningThumb(true);
                 } else {
                     setElapsedTime(newElapsedTime);
                 }
@@ -42,15 +48,18 @@ const TimeLineBlock = () => {
         } else {
             clearInterval(intervalIdRef.current);
         }
+
         return () => clearInterval(intervalIdRef.current);
-    }, [isRunning, elapsedTime, totalTime]);
+
+    }, [isRunningThumb, elapsedTime, totalTime]);
+
 
     const handleStartButtonClick = () => {
-        setIsRunning(prevIsRunning => !prevIsRunning);
+        setIsRunningThumb(prevIsRunning => !prevIsRunning);
     };
     const handleStopButtonClick = () => {
         clearInterval(intervalIdRef.current);
-        setIsRunning(false);
+        setIsRunningThumb(false);
         setElapsedTime(0);
     };
 
@@ -62,7 +71,6 @@ const TimeLineBlock = () => {
                 ticks.push(
                 <div key={i} className="tick" style={{ left: `${leftPosition}px` }}>
                     <span style={{marginLeft: 5}}>{i}s</span>
-
                 </div>
             );
             // Добавляем дополнительные метки между основными делениями
@@ -77,8 +85,8 @@ const TimeLineBlock = () => {
         return ticks;
     };
     const handleThumbDragStart = (event) => {
-        event.preventDefault();
-        setIsRunning(false); // Остановить таймер во время перемещения ползунка
+        // event.preventDefault();
+        setIsRunningThumb(false); // Остановить таймер во время перемещения ползунка
         document.addEventListener('mousemove', handleThumbDrag);
         document.addEventListener('mouseup', handleThumbDragEnd);
     };
@@ -95,24 +103,93 @@ const TimeLineBlock = () => {
         document.removeEventListener('mouseup', handleThumbDragEnd);
 
     };
+
+
+    const handleAddKeyClick = () => {
+        const newKey = {
+            id: toolBlockState.keyCount,
+            name: `Key${toolBlockState.keyCount}`,
+            isActive: false,
+            position: 0 // Установим начальную позицию для нового ключа
+        };
+        toolBlockState.addKey(); // Увеличиваем счетчик ключей в toolBlockState
+        setKeys([...keys, newKey]); // Добавляем новый ключ в состояние
+    };
+
+    const [draggingKeyId, setDraggingKeyId]= useState(null)
+    const handleKeyMouseDown = (event, keyId) => {
+        setIsDraggingKey(true); // Устанавливаем флаг, что началось перетаскивание ключа
+        setDraggingKeyId(keyId); // Устанавливаем id перетаскиваемого ключа
+        const updatedKeys = keys.map(key => {
+            if (key.id === keyId) {
+                return { ...key, isActive: true };
+            }
+            return { ...key, isActive: false }; // Сбрасываем все остальные ключи в неактивное состояние
+        });
+
+        // Обновляем состояние keys новым массивом
+        setKeys(updatedKeys);
+    };
+
+    const handleKeyDrag = (event) => {
+        console.log(isDraggingKey)
+        if (isDraggingKey) {
+            const boundingRect = timelineKeyRef.current.getBoundingClientRect();
+            const newPosition = event.clientX - boundingRect.left;
+            // Обновляем позицию перетаскиваемого ключа
+            const updatedKeys = keys.map(key => {
+                if (key.id === draggingKeyId) {
+                    return { ...key, position: newPosition };
+                }
+                return key;
+            });
+            setKeys(updatedKeys);
+        }
+    };
+
+    const handleKeyEnd = () => {
+        setIsDraggingKey(false); // Сбрасываем флаг перетаскивания ключа
+        document.removeEventListener('mousemove', handleKeyDrag);
+    };
+
     return (
         <div className="timeline-block">
-            <div className="timeline-player">
-                <div >
-                    <button className="btn left-stop-button" id="leftStopBtn" onClick={handleStopButtonClick}></button>
-                    <button className={isRunning ? 'btn pause-button' : 'btn play-button'} id="playBtn" onClick={handleStartButtonClick}></button>
+            <div className="timeline-left">
+                <div className="timeline-controls">
+                    <div className="timeline-player">
+                        <button className="btn left-stop-button" id="leftStopBtn" onClick={handleStopButtonClick}></button>
+                        <button className={isRunningThumb ? 'btn pause-button' : 'btn play-button'} id="playBtn" onClick={handleStartButtonClick}></button>
+                    </div>
+                    <div className="timer">{formatTime(elapsedTime)} </div>
+                </div>
+                <div className="timeline-animation-tool">
+                    <div className="btn-key add"  onClick={handleAddKeyClick}></div>
+                </div>
+            </div>
+            <div className="timeline-right" >
+                <div className="timeline-ticks" ref={timelineRef} onMouseDown={handleThumbDragStart}>
+                    {renderTimelineTicks()}
+                    <div className="thumb-end-time" ref={thumbEndTimeRef} style={{ left: `${thumbPosition}px` }}></div>
+                </div>
+                <div className="timeline-key-frames">
+                    <div className="thumb-current" ref={thumbCurrent} style={{ left: `${((elapsedTime/1000) * 150)}px` }}></div>
+                    <div className="timeline-line" ref={timelineKeyRef} onMouseMove={handleKeyDrag} onMouseUp={handleKeyEnd}>
+                        {keys.map(key => (
+                            <div
+                                onMouseDown={(event) => handleKeyMouseDown(event, key.id)}
+                                key={key.name}
+                                id={key.name}
+                                className={`btn-key frame ${key.isActive ? 'active' : ''}`}
+                                style={{ left: `${key.position}px` }} // Используем позицию из состояния
+                            ></div>
+                        ))}
+                    </div>
+                </div>
                 </div>
 
-                <div className="timer">{formatTime(elapsedTime)} </div>
-            </div>
-            <div className="timeline" ref={timelineRef} onMouseDown={handleThumbDragStart}>
-                {renderTimelineTicks()}
-                <div className="thumb-end-time" ref={thumbEndTimeRef} style={{ left: `${thumbPosition}px` }}></div>
-                <div className="thumb-current" ref={thumbCurrent} style={{ left: `${((elapsedTime/1000) * 150)}px` }}></div>
-            </div>
 
         </div>
     );
-};
+});
 
 export default TimeLineBlock;
---
+++ client/src/store/toolBlockState.js
@@ -0,0 +1,19 @@
+import {makeAutoObservable} from "mobx";
+import {useRef} from "react";
+
+class toolBlockState{
+    constructor() {
+        makeAutoObservable(this)
+        this.keyCount = 0
+        this.keys = []
+
+    }
+
+    addKey(){
+        this.keys.push({id: this.keyCount, name: `Key${this.keyCount}`, isActive: false, position: 0});
+        this.keyCount++
+        // console.log(keys)
+    }
+}
+
+export default new toolBlockState()
