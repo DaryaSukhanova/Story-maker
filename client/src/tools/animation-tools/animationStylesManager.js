aa3b2b198506557dcbb77746222a9ef4f4db41bb
Sukhanova Darya <daria_15tit@mail.ru>
2024-05-09 02:57:52 +0300

    moving along a curve in a BookEditor with bugs

--- client/src/tools/animation-tools/animationStylesManager.js
+++ client/src/tools/animation-tools/animationStylesManager.js
@@ -11,24 +11,27 @@ export const createAnimationStyle = (origin, keys, index, animDuration, animName
 
     let keyframes = `
     0% {
-        transform: rotate(0deg) ;
+        transform-origin: ${x}px ${y}px;
+        transform: rotate(0deg) scale(1, 1) translate(0px, 0px) skew(0deg, 0deg);
     }
     `;
 
-    // keys.forEach((key, index) => {
-    //     const percent = (key.duration / animDuration)*100;
-    //     keyframes += `
-    //     ${percent}% {
-    //         transform: rotate(${key.rotate}deg);
-    //     }
-    // `;
-    // });
+    keys.forEach((key, index) => {
+        const percent = (key.duration / animDuration)*100;
+        keyframes += `
+        ${percent}% {
+            transform-origin: ${x}px ${y}px;
+            transform: rotate(${key.rotate}deg) scale(${key.scaleX}, ${key.scaleY}) translate(${key.translateX}px, ${key.translateY}px) skew(${key.skewX}deg, ${key.skewY}deg);
+        }
+    `;
+    });
     const maxDurationKey = keys.reduce((maxKey, currentKey) => {
         return currentKey.duration > maxKey.duration ? currentKey : maxKey;
     }, keys[0]); // Начальное значение - первый ключ
     keyframes += `
     100% {
-        transform: rotate(${maxDurationKey.rotate}deg);
+        transform-origin: ${x}px ${y}px;
+        transform: rotate(${maxDurationKey.rotate}deg) scale(${maxDurationKey.scaleX}, ${maxDurationKey.scaleY}) translate(${maxDurationKey.translateX}px, ${maxDurationKey.translateY}px) skew(${maxDurationKey.skewX}deg, ${maxDurationKey.skewY}deg);
     }
     `;
     style.textContent = `
