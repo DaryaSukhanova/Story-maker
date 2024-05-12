0820caf28ba0e0e57f43dfbb5ac1e0503e8bf81a
Elena B <brileva.2002@mail.ru>
2024-05-12 14:06:06 +0300

    added page loading

--- client/src/actions/page.js
+++ client/src/actions/page.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import pageState from "../store/pageState";
+import { printAnimation, printBackground } from "./file";
 
 export const savePage = async(name, closeModal) => {
     closeModal();
@@ -22,4 +23,25 @@ export const savePage = async(name, closeModal) => {
     } catch (error) {
         console.error('Error saving page:', error);
     }
+}
+
+export const loadPage = async(file) => {
+    try {
+		const response = await axios.get(`http://localhost:5000/api/v1/pages?id=${file._id}`, {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`
+			}
+		})
+        for (const item of response.data.backgrounds) {
+            printBackground(item)
+        }
+        for (const item of response.data.animations) {
+            printAnimation(item)
+        } 
+
+        console.log("Page successfully added to the canvas");
+    } catch (e) {
+        console.error("Error fetching page data:", e?.response?.data?.message);
+    }
+
 }
